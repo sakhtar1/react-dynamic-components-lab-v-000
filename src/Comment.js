cd1ebe3acf6bf8375db3d0b0e7940630dfebaf4f
Sharmina Akhtar <sakht318@gmail.com>
2019-02-19 04:22:05 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,14 +1,14 @@
 // add Comment component here
 import React, { Component } from 'react'
-import BlogPost from './BlogPost'
+
 
 export default class Comment extends Component {
 
   render() {
-  
+
     return (
       <div className="color-box" style={{opacity: 2}}>
-        <BlogPost />
+        {this.props.commentText}
       </div>
     )
   }
